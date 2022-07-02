4d9a5428129b55bf7fd5c33a7dd141abc0f908de
Dawit Nigus <101523835+Dawit-Nigus@users.noreply.github.com>
2022-07-02 20:01:02 +0300

    RMS

---
+++ Restaurant_management_system/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+from django.contrib.auth import views as auth_views
+from django.views.static import serve
+from django.conf.urls import url
+
+urlpatterns = [
+    path('', views.index,  name='index'),
+    path('admin/', admin.site.urls),
+    path('shop/', include('shop.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
