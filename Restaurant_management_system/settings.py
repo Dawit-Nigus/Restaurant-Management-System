31efb35bd47227ff23dc9c2ffe51cb485aebc9f0
Dawit Nigus <101523835+Dawit-Nigus@users.noreply.github.com>
2022-07-03 13:25:16 +0300

    RMS

--- Restaurant_management_system/settings.py
+++ Restaurant_management_system/settings.py
@@ -141,5 +141,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'xxxx@gmail.com'   # enter you email address
-EMAIL_HOST_PASSWORD = 'xxxx'     # enter your password 
+EMAIL_HOST_USER = 'dawitnigus18@gmail.com'   # enter you email address
+EMAIL_HOST_PASSWORD = 'ntyyjpagzhqazcyt'     # enter your password 
+
